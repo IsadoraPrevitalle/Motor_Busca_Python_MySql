12e77bc723287ffd8873a65e8bf938e5ed1e38cf
Data Science <104457205+IsadoraPrevitalle@users.noreply.github.com>
2023-08-27 12:04:14 -0300

    Update CRUD_MotorBusca.py

--- CRUD_MotorBusca.py
+++ CRUD_MotorBusca.py
@@ -1,101 +1,137 @@
-#Importando bibliotecas
-import mysql.connector                                                                                      #Conexão com o banco de dados
-import re                                                                                                   #Expressões regulares para texto
-import numpy as np                                                                                          #Manipulação de matrizes 
-import pandas as pd                                                                                         #Manipulação de dataframe
+import mysql.connector
+import numpy as np
+import pandas as pd
+import re
+import tkinter as tk
+from tkinter import ttk
 
-#Conexão com MySQL
-conexao = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='123456',
-    database='targetech',
-)
+class DatabaseInterface:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("interface de busca")
+        self.host = 'localhost'
+        self.user = 'root'
+        self.password = '123456'
+        self.database = 'targetech'
+        self.create_connection()
+        self.create_interface()
+    
+    def create_connection(self):
+        self.conexao = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.conexao.cursor()
 
-#Declaração do cursor - responsavel pela busca linha a linha no BD
-cursor = conexao.cursor()
+    def create_interface(self):
+        #self.label = ttk.Label(self.root, text="Insira o dado que deseja encontrar:")
+        #self.label.pack()
 
-#                                                              #Entrada e tratamento de dados do usuario
-#Dados para pesquisa                                                                
-valor = str(input("\nInsira o dado que deseja encontrar: "))
+        self.entry = ttk.Entry(self.root)
+        self.entry.pack()
 
-#declaração de variaveis que auxiliará no tratamento dos dados de entrada
-buscaPal = re.split(r'\s+',valor)                                                                           #Divide o texto em palavras
-tipo_busca = str()                                                                                          #Variavel do tipo da busca 
-mes_busca = str()                                                                                           #Varivel do periodo de busca
-operacao = buscaPal[0]                                                                                      #Operações de busca
-linha = int()                                                                                               #Varivel de quantidade de busca
-linha_posicao = str()                                                                                       #variavel de ordem da busca
-lista = list()                                                                                              #Declaracao de lista
-cont = 0
+        self.search_button = ttk.Button(self.root, text="Buscar", command=self.perform_search)
+        self.search_button.pack()
 
-#Busca de expressões na frase - locação nas variaveis 
-for i in buscaPal:
-    var = i
-    if var == 'de':
-        tipo_busca = buscaPal[cont+1]
-    elif var == 'em':
-        mes_busca = buscaPal[cont+1]
-    elif var == 'primeiros' or var == 'ultimos':
-        linha = int(buscaPal[cont-1])
-        linha_posicao = var
-    cont += 1
+        self.result_text = tk.Text(self.root)
+        self.result_text.pack()
 
-#                                                                    #Buscas no banco de dados
-#Todos os registros em um determinado mês                                                                         
-if operacao == "todos" or operacao == 'tudo':
-    comando = f'SELECT * FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%"'
-    colunas = ["Ano", "Mês", "Total","Worm", "DOS", "Invasão", "Web", "Scan", "Fraude", "Outros"]
-    cursor.execute(comando)
-    resul = cursor.fetchall()
-    #loop para exibição do resultado em uma matriz
-    for i in resul:
-        conv = list(i)
-        lista.append(conv)
-    matriz = np.array(lista)
-    #Transformação da matriz em cvs
-    df = pd.DataFrame(matriz, columns=colunas)
-    print("\nResultados da Busca:\n")
-    print(df.head())
-    cursor.close()
-    conexao.close()
-#Buscas especificas
-else:
-    #Soma de um tipo de registros em um determinado mês nos anos de 2010 a 2019
-    #                                                                                                                     #"Soma de Tipo_registro em mes_busca"
-    if operacao == "soma":
-        comando = f'SELECT Ano, Mes, Total, SUM({tipo_busca}) FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" GROUP BY {tipo_busca}, ano, mes, total'
-    #Menor valor de um tipo de registro em um determinado mês nos anos de 2010 a 2019
-    #                                                                                                                   #"Minimo de Tipo_registro em mes_busca"
-    elif operacao == "minimo":
-        comando = f'SELECT Ano, Mes, Total, MIN({tipo_busca}) FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" GROUP BY {tipo_busca}, ano, mes, total'
-    #Maior valor de um tipo de registro em um determinado mês nos anos de 2010 a 2019
-    #                                                                                                                   #"Maximo de Tipo_registro em mes_busca"
-    elif operacao == "maximo":
-        comando = f'SELECT Ano, Mes, Total, MAX({tipo_busca}) FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" GROUP BY {tipo_busca}, ano, mes, total'
-    #Quntidade de registros nos últimos anos de um tipo em um determinado mês no periodo dos anos de 2010 a 2019- 
-    #                                                                                                    #"3 primeiros_registros de Tipo_registro em mes_busca"
-    elif linha_posicao == 'primeiros':
-        comando = f'SELECT Ano, Mes, Total, {tipo_busca} FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" ORDER BY Ano desc LIMIT {linha}'
-    #Quntidade de registros nos primeiros anos de um tipo em um determinado mês no periodo dos anos de 2010 a 2019- 
-    #                                                                                                      #"3 ultimos_registros de Tipo_registro em mes_busca"
-    elif linha_posicao == 'ultimos':
-        comando = f'SELECT Ano, Mes, Total, {tipo_busca} FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" ORDER BY Ano asc LIMIT {linha}'
-    #Quntidade de registros de um tipo em um determinado mês no periodo dos anos de 2010 a 2019- 
-    #                                                                                                          #"Todos_registros de Tipo_registro em mes_busca"
-    else:
-        comando = f'SELECT Ano, Mes, Total, {tipo_busca} FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%"'
-    colunas = ["Ano", "Mês", "Total", tipo_busca]
-    cursor.execute(comando)
-    resul = cursor.fetchall()
-    #loop para exibição do resultado em uma matriz
-    for i in resul:
-        conv = list(i)
-        lista.append(conv)
-    matriz = np.array(lista)
-    #Transformação da matriz em cvs
-    df = pd.DataFrame(matriz, columns=colunas)
-    print("\nResultados da Busca:\n")
-    print(df)
-    cursor.close()
-    conexao.close()
+    def perform_search(self):
+        valor = self.entry.get()
+        buscaPal = re.split(r'\s+', valor)
+        tipo_busca = str()
+        mes_busca = str()
+        operacao = buscaPal[0]
+        linha = 0
+        linha_posicao = str()
+        lista = list()
+
+        for i in buscaPal:
+            var = i
+            if var == 'de':
+                tipo_busca = buscaPal[buscaPal.index(var) + 1]
+            elif var == 'em':
+                mes_busca = buscaPal[buscaPal.index(var) + 1]
+            elif var == 'primeiros' or var == 'ultimos':
+                linha = int(buscaPal[buscaPal.index(var) - 1])
+                linha_posicao = var
+        try:
+            if operacao == "todos" or operacao == 'tudo':
+                if mes_busca:
+                    comando = f'SELECT * FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%"'
+                else:
+                    comando = f'SELECT * FROM targetech.fraudes'
+                colunas = ["Ano", "Mês", "Total","Worm", "DOS", "Invasão", "Web", "Scan", "Fraude", "Outros"]
+                self.cursor.execute(comando)
+                resul = self.cursor.fetchall()
+
+                result_text = "Resultados da Busca:\n"
+                for row in resul:
+                    conv = list(row)
+                    lista.append(conv)
+                    matriz = np.array(lista)
+                df = pd.DataFrame(matriz, columns=colunas)
+                result_text = df
+                self.result_text.delete("1.0", tk.END)
+                self.result_text.insert(tk.END, result_text)
+
+            else:
+                if operacao == "soma":
+                    if mes_busca:
+                        comando = f'SELECT Ano, Mes, Total, SUM({tipo_busca}) FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" GROUP BY {tipo_busca}, ano, mes, total'
+                    else:
+                        comando = f'SELECT Ano, Mes, Total, SUM({tipo_busca}) FROM targetech.fraudes GROUP BY {tipo_busca}, ano, mes, total'  
+                elif operacao == "minimo":
+                    if mes_busca:
+                        comando = f'SELECT Ano, Mes, Total, MIN({tipo_busca}) FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" GROUP BY {tipo_busca}, ano, mes, total'
+                    else:
+                        comando = f'SELECT Ano, Mes, Total, MIN({tipo_busca}) FROM targetech.fraudes GROUP BY {tipo_busca}, ano, mes, total'  
+                elif operacao == "maximo":
+                    if mes_busca:
+                        comando = f'SELECT Ano, Mes, Total, MAX({tipo_busca}) FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" GROUP BY {tipo_busca}, ano, mes, total'
+                    else:
+                        comando = f'SELECT Ano, Mes, Total, MAX({tipo_busca}) FROM targetech.fraudes GROUP BY {tipo_busca}, ano, mes, total'  
+                elif linha_posicao == 'primeiros':
+                    if mes_busca:
+                        comando = f'SELECT Ano, Mes, Total, {tipo_busca} FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" ORDER BY Ano DESC LIMIT {linha}'
+                    else:
+                        comando = f'SELECT Ano, Mes, Total, {tipo_busca} FROM targetech.fraudes ORDER BY Ano DESC LIMIT {linha}'
+                elif linha_posicao == 'ultimos':
+                    if mes_busca:
+                        comando = f'SELECT Ano, Mes, Total, {tipo_busca} FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%" ORDER BY Ano ASC LIMIT {linha}'
+                    else:
+                        comando = f'SELECT Ano, Mes, Total, {tipo_busca} FROM targetech.fraudes ORDER BY Ano ASC LIMIT {linha}'
+                elif operacao:
+                    if mes_busca:
+                        comando = f'SELECT Ano, Mes, Total, {operacao} FROM targetech.fraudes WHERE Mes LIKE "%{mes_busca}%"'
+                    else:
+                       comando = f'SELECT Ano, Mes, Total, {operacao} FROM targetech.fraudes'
+                
+                colunas = ["Ano", "Mês", "Total", tipo_busca or operacao]
+                self.cursor.execute(comando)
+                resul = self.cursor.fetchall()
+
+                result_text = "Resultados da Busca:\n"
+                for row in resul:
+                    conv = list(row)
+                    lista.append(conv)
+                    matriz = np.array(lista)
+                df = pd.DataFrame(matriz, columns=colunas)
+                result_text = df
+                self.result_text.delete("1.0", tk.END)
+                self.result_text.insert(tk.END, result_text)
+
+        except mysql.connector.errors.ProgrammingError as e:
+            result_text = "\nDesculpe, não encontramos esse dado em nossa base. \nPor favor tente a busca com outro valor :)"
+            self.result_text.delete("1.0", tk.END)
+            self.result_text.insert(tk.END, result_text)
+    def close_connection(self):
+        self.cursor.close()
+        self.conexao.close()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    interface = DatabaseInterface(root)
+    root.mainloop()
